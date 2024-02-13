c4a5639f6ccdf2f2229287dea1bdebe356ec770d
Muhsin PK <shahalashalu681@gmail.com>
2024-02-13 23:16:43 +0530

    initial commit

---
+++ src/first.js
@@ -0,0 +1,29 @@
+
+import React from 'react';
+function First() {
+   const containerStyle = {
+        backgroundColor: 'black',
+        color: 'white', // You can adjust text color as needed
+        minHeight: '100vh', // Ensure the container takes at least the full height of the viewport
+    };
+   
+    
+  
+      return (
+           <div style={containerStyle}>
+      <title>Centered Heading</title>
+      <h1>Epic Games: An American Video Game And Software Developer And Publisher Based In Cari, North Carolina</h1>
+      <img src="C:\Users\muhsi\Downloads\fortnite.jpeg" alt="Your Image Description"></img>
+ <p>Create play and battle with friends for free in fortnite,Be the last player standing in fortnite Battle Royale and zero build ,
+  experience a concert or line event,or discover over a million creator made games,including racing parkour,zombie survival and more.
+  Each Fortnite Island has an individual age rating so you can find the one that's right for you and your friends.Find it all in fortnite</p>
+  <button>
+   visit website
+</button>
+
+    </div>
+    
+  );
+
+};
+export default First;
